c6d8147c1b24230f6b6e42ca4ede090a7663a009
Mark Delgado <markdelgado.md@hotmail.com>
2022-09-03 12:53:58 -0400

    commit tree da853d38744616970fe378351fb5bafa068fc7de

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,16 +8,19 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+     rescue ActiveRecord::RecordInvalid => invalid
+      render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  
   end
 
   private
 
+  
   def post_params
     params.permit(:category, :content, :title)
   end
 
 end
+
